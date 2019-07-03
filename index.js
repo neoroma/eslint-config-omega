675b48ddf393c355ea91e1dc86bb01ebd1577622
Roman Shestakov <neoroma@gmail.com>
2019-07-03 11:42:39 +0300

    Fix some rules to work well with prettier

--- index.js
+++ index.js
@@ -1,81 +1,89 @@
 module.exports = {
-  "env": {
-    "node": true,
-    "jest": true
+  'env': {
+    'node': true,
+    'jest': true,
   },
-  "parserOptions": {
-    "ecmaVersion": 8,
-    "sourceType": "module"
+  'parserOptions': {
+    'ecmaVersion': 2018,
+    'sourceType': 'module',
   },
-  "extends": [
-    "eslint:recommended",
-    "standard"
+  'extends': [
+    'eslint:recommended',
+    'standard',
   ],
-  "plugins": [
-    "react-hooks"
+  'plugins': [
+    'react-hooks',
   ],
-  "rules": {
-    "arrow-parens": [
-      "error",
-      "always"
+  'rules': {
+    'arrow-parens': [
+      'error',
+      'as-needed',
     ],
-    "import/imports-first": 0,
-    "import/newline-after-import": 0,
-    "import/no-named-as-default": 0,
-    "import/no-unresolved": "off",
-    "indent": [
+    'space-before-function-paren': [
+      'error',
+      {
+        'anonymous': 'always',
+        'named': 'never',
+        'asyncArrow': 'always',
+      },
+    ],
+    'import/imports-first': 0,
+    'import/newline-after-import': 0,
+    'import/no-named-as-default': 0,
+    'import/no-unresolved': 'off',
+    'indent': [
       2,
       2,
       {
-        "SwitchCase": 1
-      }
+        'SwitchCase': 1,
+      },
     ],
-    "newline-per-chained-call": 0,
-    "no-unexpected-multiline": "error",
-    "semi": [
-      "error",
-      "never"
+    'newline-per-chained-call': 0,
+    'no-unexpected-multiline': 'error',
+    'semi': [
+      'error',
+      'never',
     ],
-    "no-console": 1,
-    "no-use-before-define": 0,
-    "prefer-template": 2,
-    "class-methods-use-this": 0,
-    "require-yield": 0,
-    "import/no-webpack-loader-syntax": 0,
-    "max-len": [
-      "error",
-      120
+    'no-console': 1,
+    'no-use-before-define': 0,
+    'prefer-template': 2,
+    'class-methods-use-this': 0,
+    'require-yield': 0,
+    'import/no-webpack-loader-syntax': 0,
+    'max-len': [
+      'error',
+      120,
     ],
-    "arrow-body-style": [
-      "error",
-      "as-needed"
+    'arrow-body-style': [
+      'error',
+      'as-needed',
     ],
-    "import/extensions": "off",
-    "no-case-declarations": "off",
-    "linebreak-style": [
-      "error",
-      "unix"
+    'import/extensions': 'off',
+    'no-case-declarations': 'off',
+    'linebreak-style': [
+      'error',
+      'unix',
     ],
-    "quotes": [
-      "error",
-      "single"
+    'quotes': [
+      'error',
+      'single',
     ],
-    "react/forbid-prop-types": "off",
-    "comma-dangle": "off",
-    "padded-blocks": "off",
-    "no-confusing-arrow": "off",
-    "object-curly-newline": "off",
-    "react/jsx-filename-extension": "off",
-    "import/prefer-default-export": "off",
-    "import/no-dynamic-require": "off",
-    "import/no-extraneous-dependencies": "off",
-    "max-lines": [
-      "error",
+    'react/forbid-prop-types': 'off',
+    'comma-dangle': 'off',
+    'padded-blocks': 'off',
+    'no-confusing-arrow': 'off',
+    'object-curly-newline': 'off',
+    'react/jsx-filename-extension': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-dynamic-require': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'max-lines': [
+      'error',
       {
-        "skipBlankLines": true
-      }
+        'skipBlankLines': true,
+      },
     ],
-    "react-hooks/rules-of-hooks": "error"
-    "react-hooks/exhaustive-deps": "warn",
-  }
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
 }
